6b5690e78dbbd200eb2b76acc7782cec533a601a
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2022-12-01 00:29:35 +0100

    Za Warudo

---
+++ AftelTimer.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    var minute = 9;
+    var sec = 59;
+    setInterval(function() {
+      document.getElementById("timer").innerHTML = minute + ":" + sec + " tijd over om tickets te bestellen";
+      sec--;
+      if (sec == 00) {
+        minute --;
+        sec = 60;
+        if (minute == 0) {
+          minute = 5;
+        }
+      }
+    }, 1000);
+  }
+  
