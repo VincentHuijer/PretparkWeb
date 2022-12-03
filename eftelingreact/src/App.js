53cfa4feb5a68820d47f5ac978b8749b53cfbab8
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2022-12-03 13:11:48 +0100

    Update App.js

--- eftelingreact/src/App.js
+++ eftelingreact/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css"; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => { //Als ik hier nu <Person /> toon krijg je alles van de persoon te zien.
+ const[counter, setCounter] = useState(0); //wanneer je iets aanroept als een functie en het start met 'use' in React noemen we dat een 'hook'
+ //de eerste parameter in [] zal de naam zijn van de 'state'
+
+
+
+// useEffect = deze code word uitgevoerd wanneer de pagina laad
+
+//voorbeeld1.
+/////////////////useEffect(() => {setCounter(100);},[]); 
+ //zonder tweede parameter [] (dependency array) word het constant uitgevoerd (op 100 gezet). 
+ //als er een tweede parameter [] is (de dependency array is leeg) gebeurt de uitvoer van de eerste parameter maar 1x.
+
+
+//voorbeeld 2.
+//////////////useEffect(() => {alert("You've changed the counter to "+ counter)}); 
+
+
+  return(
+  <div className="App">
+    <button onClick={() => setCounter((prevCount)=> prevCount -1)}>-</button>
+    <h1>{counter}</h1>
+    <button onClick={() => setCounter((prevCount)=> prevCount +1)}>+</button>
     </div>
-  );
-}
+    );
+};
 
 export default App;
